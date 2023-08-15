6d2d58b751129a7833a64cee09867bf948b05163
qkrehf2 <64183602+qkrehf2@users.noreply.github.com>
2023-08-15 23:57:00 +0900

    Fixed mouseItem command

--- src/main/java/org/karn/usefulcommand/commands/MouseItem.java
+++ src/main/java/org/karn/usefulcommand/commands/MouseItem.java
@@ -3,7 +3,6 @@ package org.karn.usefulcommand.commands;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import net.minecraft.command.argument.EntityArgumentType;
-import net.minecraft.network.packet.c2s.play.UpdateSelectedSlotC2SPacket;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -16,11 +15,11 @@ public class MouseItem {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         dispatcher.register(literal("hotbar")
                 .requires(source -> source.hasPermissionLevel(2))
-                .then(argument("entity", EntityArgumentType.entity())
+                .then(argument("player", EntityArgumentType.player())
                         .then(CommandManager.literal("set")
                             .then(argument("num", IntegerArgumentType.integer(0,8))
                                 .executes(ctx -> {
-                                    return hotbarSet(ctx.getSource(), ctx.getSource().getPlayer(), ctx.getArgument("num",Integer.class));
+                                    return hotbarSet(ctx.getSource(), EntityArgumentType.getPlayer(ctx, "player"), ctx.getArgument("num",Integer.class));
                                 })
                             )
                         )
